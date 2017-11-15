9003aed557ade7c3378b6d3ed687284511634293
Patrick Mutwiri <patwiri@gmail.com>
2017-11-15 15:49:17 +0300

    with Products

--- catalog/controller/module/tabcategories.php
+++ catalog/controller/module/tabcategories.php
@@ -33,9 +33,109 @@ class ControllerModuleTabCategories extends Controller {
             $url .= '&limit=' . $this->request->get['limit'];
         }
 
+        if (isset($this->request->get['filter'])) {
+            $filter = $this->request->get['filter'];
+        } else {
+            $filter = '';
+        }
+
+        if (isset($this->request->get['sort'])) {
+            $sort = $this->request->get['sort'];
+        } else {
+            $sort = 'p.sort_order';
+        }
+
+        if (isset($this->request->get['order'])) {
+            $order = $this->request->get['order'];
+        } else {
+            $order = 'ASC';
+        }
+
+        if (isset($this->request->get['page'])) {
+            $page = $this->request->get['page'];
+        } else {
+            $page = 1;
+        }
+
+        if (isset($this->request->get['limit'])) {
+            $limit = $this->request->get['limit'];
+        } else {
+            $limit = $this->config->get('config_product_limit');
+        }
+
         $data['categories'] = array();
         $results = $this->model_catalog_category->getCategories($id);
         foreach ($results as $key => $result) {
+            //products
+            $data['products'] = array();
+            $filter_data = array(
+                'filter_category_id' => $result['category_id'],
+                'filter_filter'      => $filter,
+                'sort'               => $sort,
+                'order'              => $order,
+                'start'              => ($page - 1) * $limit,
+                'limit'              => $limit
+            );
+
+            $product_total = $this->model_catalog_product->getTotalProducts($filter_data);
+
+            $results = $this->model_catalog_product->getProducts($filter_data);
+
+            foreach ($results as $result) {
+                if ($result['image']) {
+                    $image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+                } else {
+                    $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+                }
+
+                if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
+                    $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')));
+                } else {
+                    $price = false;
+                }
+
+                if ((float)$result['special']) {
+                    $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')));
+                } else {
+                    $special = false;
+                }
+
+                if ($this->config->get('config_tax')) {
+                    $tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price']);
+                } else {
+                    $tax = false;
+                }
+
+                if ($this->config->get('config_review_status')) {
+                    $rating = (int)$result['rating'];
+                } else {
+                    $rating = false;
+                }
+                $discounts = $this->model_catalog_product->getProductDiscounts($result['product_id']);
+                $product_discounts = array(); 
+                foreach ($discounts as $discount) {
+                    $product_discounts[] = array(
+                        'quantity' => $discount['quantity'],
+                        'price'    => $this->currency->format($this->tax->calculate($discount['price'], $product_info['tax_class_id'], $this->config->get('config_tax')))
+                    );
+                }
+                $data['products'][] = array(
+                    'product_id'    => $result['product_id'],
+                    'thumb'         => $image,
+                    'name'          => $result['name'],
+                    'startdate'     => $result['date_added'],
+                    'description'   => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, 270, $this->config->get('config_product_description_length')) . '..',
+                    'price'         => $price,
+                    'discounts'     => $product_discounts,
+                    'special'     => $special,
+                    'tax'         => $tax,
+                    'rating'      => $result['rating'],
+                    'href'        => $this->url->link('product/product', 'path=' . $this->request->get['path'] . '&product_id=' . $result['product_id'] . $url),
+                    'href_qv'     => $this->url->link('product/quickview', 'product_id=' . $result['product_id'])
+                );
+            }
+
+            //products
             if ($result['image']) {
                 $result['thumb'] = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_category_width'), $this->config->get('config_image_category_height'));
             } else {
@@ -51,7 +151,8 @@ class ControllerModuleTabCategories extends Controller {
                 'name'  => $result['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
                 'href'  => $this->url->link('product/category', 'path=' . $this->request->get['path'] . '_' . $result['category_id'] . $url),
                 'thumb' => $result['thumb'],
-                'category_id' => $result['category_id']
+                'category_id' => $result['category_id'],
+                'products' => $products;
             );
         }
 
